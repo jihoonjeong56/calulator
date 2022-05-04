8c955b3870853acb1b128349f2cff29ca6f52386
jihoonjeong56 <100738560+jihoonjeong56@users.noreply.github.com>
2022-05-04 10:04:04 +0900

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data = True,
+  name='00100',
+  version='0.0.1',
+  description='oss-dev my calculator',
+  author='jihoonjeong',
+  author_email='jeong283056@gmail.com',
+  url = "https://github.com/jihoonjeong56/00100",
+  download_url = "https://github.com/jihoonjeong56/00100/archive/refs/tags/v0.0.1.zip",
+  install_requires=['pytest'],
+  long_description = 'oss-dev calculator python module',
+  long_description_content_type = 'text/markdown',
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ],
+)
